a6730ddb41161a52e60319768ed79354256666c1
WUStudent <wustudent@webster.edu>
2026-01-20 16:48:08 -0600

    Session 1 complete. Player ship thrusting

---
+++ SpaceShooterUnity/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : Ship
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Thrust();
+        }
+
+        FollowMouse(); 
+    }
+
+    void FollowMouse()
+    {
+        //Step 1: Find out where mouse cursor is relative to camera and screen
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
+
+        //Step 2: Determine the direction between the ship and the mouse cursor
+        Vector2 directionToFace = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        //Step 3: Make the ship actually point toward the mouse cursor
+        transform.up = directionToFace;
+    }
+}
---
+++ SpaceShooterUnity/Assets/Scripts/Ship.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public float acceleration;
+    public float currentSpeed;
+    public float maxSpeed;
+
+    public float fireRate;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Thrust()
+    {
+        rb.AddForce(transform.up * acceleration);
+    }
+
+    public void PewPew()
+    { 
+    
+    }
+
+    public void TakeDamage()
+    { 
+    
+    }
+
+    public void Explode()
+    { 
+    
+    }
+}
