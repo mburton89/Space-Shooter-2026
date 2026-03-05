2dc0eb6211d41c7961c233f5e788239f29585870
StarlightFS <159479047+StarlightFS@users.noreply.github.com>
2026-03-05 16:32:49 -0600

    added music, made projectiles look better, might be finished

--- SpaceShooterUnity/Assets/Scripts/MainMenu.cs
+++ SpaceShooterUnity/Assets/Scripts/MainMenu.cs
@@ -7,16 +7,25 @@ public class MainMenu : MonoBehaviour
 {
     public Button startButton;
 
+    public AudioSource musicAudioSource;
+    public AudioClip musicClipToLoop;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(HandleStartButtonClicked);
+
+        musicAudioSource.clip = musicClipToLoop;
+
+        musicAudioSource.loop = true;
+        musicAudioSource.Play();
+        
+
     }
 
     void HandleStartButtonClicked()
     {
         SceneManager.LoadScene(1);
     }
-    
+  
 }
