b5b22cfe76dd0ca563b577e0a0b3b26267bfc269
WUStudent <nr.cruzcht@gmail.com>
2026-03-05 14:48:55 -0600

    Fixed Healing bulb bug

--- SpaceShooterUnity/Assets/Scripts/HealingBulb.cs
+++ SpaceShooterUnity/Assets/Scripts/HealingBulb.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class HealingBulb : MonoBehaviour
 {
     public int lifeToGive;
+    public int maxLifeToGive;
 
     public GameObject friend;
-    
+    public AudioClip heal;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       
 
-        if (collision.GetComponent<Ship>() && collision.gameObject == friend)
+
+        if (collision.GetComponent<Ship>() && collision.gameObject == friend && friend.GetComponent<PlayerShip>().currentHealth < friend.GetComponent<PlayerShip>().maxHealth)
         {
+
+
+            AudioSource.PlayClipAtPoint(heal, Camera.main.transform.position, 1f);
             collision.GetComponent<Ship>().Givelife(lifeToGive);
 
             Destroy(gameObject);
+
         }
+
     }
 
     private void Awake()
