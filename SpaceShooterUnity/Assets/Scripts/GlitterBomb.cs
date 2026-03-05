9ce63fa66d327a8a2b75c1e735e47b0b63cfa13a
WUStudent <k.riverford@outlook.com>
2026-03-05 14:47:09 -0600

    whole lotta nothin

--- SpaceShooterUnity/Assets/Scripts/GlitterBomb.cs
+++ SpaceShooterUnity/Assets/Scripts/GlitterBomb.cs
@@ -1,5 +1,7 @@
 using JetBrains.Annotations;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor.UI;
 
 public class GlitterBomb : Projectile
 {
@@ -7,22 +9,30 @@ public class GlitterBomb : Projectile
     public GameObject glitterBombExplosionPrefab;
     public Vector3 explosionPrefabRotation;
 
+    public Collider2D blastRadius;
+    public Collider2D bombBody;
+
+    private List<GameObject> inBlastRadius;
+
+    private void Start()
+    {
+        inBlastRadius = new List<GameObject>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //If we made it this far, we collided with SOMETHING
+            // Check if collider is the bomb itself
+            if (collision.GetComponent<Ship>() && collision.gameObject != firingShip)
+            {
+                //If we made it this far, the thing we collided with is a SHIP. WOOHOO! ANNNNNNDD, its not the ship that fired the projectile
 
-        Debug.Log("Collision!");
+                collision.GetComponent<Ship>().TakeDamage(damageToGive);
 
-        if (collision.GetComponent<Ship>() && collision.gameObject != firingShip)
-        {
-            //If we made it this far, the thing we collided with is a SHIP. WOOHOO! ANNNNNNDD, its not the ship that fired the projectile
+                GlitterBombExplode();
 
-            collision.GetComponent<Ship>().TakeDamage(damageToGive);
-
-            GlitterBombExplode();
-
-            Destroy(gameObject);
-        }
+                Destroy(gameObject);
+            }
     }
 
     private void GlitterBombExplode()
