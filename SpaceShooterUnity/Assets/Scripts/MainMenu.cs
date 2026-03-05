6800712a81a98652165aa605aea171f1b71b78f1
EpicSignal <110298681+EpicSignal@users.noreply.github.com>
2026-03-05 15:54:06 -0600

    Added Hellion enemy class, main menu high score display, credits scene, fixed explosion effect looping

--- SpaceShooterUnity/Assets/Scripts/MainMenu.cs
+++ SpaceShooterUnity/Assets/Scripts/MainMenu.cs
@@ -3,22 +3,40 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
-using UnityEngine;
-
 public class MainMenu : MonoBehaviour
 {
-
+    public TextMeshProUGUI recordWave;
     public Button startButton;
+    public Button creditsButton;
+    public Button backButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startButton.onClick.AddListener(HandleStartButtonClicked);
+        creditsButton.onClick.AddListener(HandleCreditsButtonClicked);
+        DisplayRecordWave(PlayerPrefs.GetInt("HighestWave"));            
+        backButton.onClick.AddListener(HandleBackButtonClicked);                                                    
+    }
+
+    public void DisplayRecordWave(int highestWave)
+    {
+        recordWave.SetText("RECORD WAVE: " + highestWave);
     }
 
     void HandleStartButtonClicked()
     {
         SceneManager.LoadScene(1);
+    }
+
+    void HandleCreditsButtonClicked()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    void HandleBackButtonClicked()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
