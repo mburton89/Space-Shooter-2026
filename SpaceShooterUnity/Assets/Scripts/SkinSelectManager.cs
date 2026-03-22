75b41d6747a10690bcf73485e44a068a5621eefc
belnatragames <anattrass03@gmail.com>
2026-03-22 16:17:42 -0500

    Skin Select Progress

--- SpaceShooterUnity/Assets/Scripts/SkinSelectManager.cs
+++ SpaceShooterUnity/Assets/Scripts/SkinSelectManager.cs
@@ -4,14 +4,33 @@ using UnityEngine.UI;
 
 public class SkinSelectManager : MonoBehaviour
 {
-    public Button continueButton; // The "Roll Out" button
-    private int selectedSkin = -1; // Track which skin the player clicked
+    public Button continueButton; // The "To the Sky" button
+    public Button[] skinButtons;  // Drag all 3 skin buttons here in order
+    public Color highlightColor = Color.yellow; // Color of the selection box
+    public float outlineWidth = 3f; // Thickness of the selection box
+
+    private int selectedSkin = -1;
+
 
     void Start()
     {
         // Hide the continue button until a skin is selected
         continueButton.gameObject.SetActive(false);
+
+
+        // Remove any existing outlines at start
+        foreach (Button btn in skinButtons)
+        {
+            Outline outline = btn.GetComponent<Outline>();
+            if (outline != null)
+            {
+                outline.enabled = false;
+            }
+        }
+
+
     }
+
 
     // Call this from your skin buttons
     public void SelectSkin(int skinIndex)
@@ -23,12 +42,27 @@ public class SkinSelectManager : MonoBehaviour
 
         // Show the continue button now that a skin is selected
         continueButton.gameObject.SetActive(true);
+
+
+        // Update the highlight box on all buttons
+        for (int i = 0; i < skinButtons.Length; i++)
+        {
+            Outline outline = skinButtons[i].GetComponent<Outline>();
+            // Add an Outline component if one doesn't exist yet
+            if (outline == null)
+            {
+                outline = skinButtons[i].gameObject.AddComponent<Outline>();
+                outline.effectColor = highlightColor;
+                outline.effectDistance = new Vector2(outlineWidth, outlineWidth);
+            }
+            // Enable outline only on the selected skin, disable on others
+            outline.enabled = (i == skinIndex);
+        }
     }
 
     // Call this from the continue button
     public void ContinueToGame()
     {
-        // Load your gameplay scene (replace 2 with your actual scene index or name)
         SceneManager.LoadScene(2);
     }
 }
